ef7dac37bfbc7aa46acd01d98f1ff08b936a4c23
Richardson94 <richcori94@gmail.com>
2023-05-29 22:17:43 -0400

    improving html folder

--- server/html/server.js
+++ server/html/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use('/assets', express.static('assets')); //middleware
+
 app.get('/', function (req, res) {
   res.sendFile('index.html', {
     root: __dirname,
   });
+  //  res.send(__dirname);
 });
 
 app.listen(3000);
