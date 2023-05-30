090290c113915110972b80077dbfeffb52ebff66
Richardson94 <richcori94@gmail.com>
2023-05-29 22:33:53 -0400

    how to improve the cache managable

--- server/html/server.js
+++ server/html/server.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use('/assets', express.static('assets')); //middleware
+app.set('view engine', 'ejs');
+
+app.use(
+  '/assets',
+  express.static('assets', {
+    etag: false,
+    maxAge: '5h',
+  })
+); //middleware
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html', {
-    root: __dirname,
-  });
+  // res.sendFile('index.html', {
+  //   root: __dirname,
+  // });
   //  res.send(__dirname);
+
+  res.render('index.ejs');
 });
 
 app.listen(3000);
